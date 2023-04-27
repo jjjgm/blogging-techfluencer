3adab18f675cf73b51c7facece2f2372e6b6bec7
jjjgm <businessjgm@icloud.com>
2023-04-27 11:16:12 -0700

    user table

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require ('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init (
+    {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [6],
+            isAlpha: true
+        }
+    }
+}, 
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+}
+);
+
+module.exports = User
