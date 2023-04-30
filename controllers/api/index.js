b15f786b426e9cccaee1265c064b13c2d6719bdc
jjjgm <businessjgm@icloud.com>
2023-04-29 23:29:12 -0700

    need to render dashboard

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/users');
+router.use('/posts');
+//USE THIS LINK FOR COMMENTS WHEN COMMENTS ARE MADE
+// router.use('/comments');
+
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
-const { User } = require('../../models/user')
+const { User } = require('../../models/user');
+const { Post } = require('../../models/post');
 
+
+// GET USER'S BLOGPOSTS BY ID
+router.get('/api/users/:id', async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user.id, {
+            include: [{ model: Post }]
+        });
+        const user = userData.get({ plain: true })
+        //GET 200 FIRST THEN RENDER
+        // res.render('dashboard', { user });
+        res.status(200).json({ user });
+    } catch (err) {
+        console.log(err);
+        res.json(err);
+    }
+    });
